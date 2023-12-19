d97c0c7dfbc2b69b67e4878b96b06cb0a0154a6d
Santosh Vallabhaneni <13214047+santoshvallabh@users.noreply.github.com>
2023-12-19 01:03:13 -0500

    Added buttons to front end UI and score display

--- javascript.js
+++ javascript.js
@@ -1,29 +1,28 @@
-function getComputerChoice () {
+function getComputerChoice() {
     const random = Math.floor(Math.random() * 3)
     switch (random) {
         case 1:
             return "ROCK"
-        break;
+            break;
         case 2:
             return "PAPER"
-        break;
+            break;
         default:
             return "SCISSORS"
-        break;
+            break;
     }
 }
 
 
-
-function playRound (playerChoice, computerChoice) {
+function playRound(playerChoice, computerChoice) {
     if (playerChoice.toUpperCase() == "ROCK") {
         switch (computerChoice) {
             case "SCISSORS":
-                return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ". Player wins!"
-            break;
+                return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ". Player wins the round!!"
+                break;
             case "PAPER":
-                return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ". Computer wins!"
-            break;
+                return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ". Computer wins the round!!"
+                break;
             default:
                 return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ". It's a tie!"
         }
@@ -32,11 +31,11 @@ function playRound (playerChoice, computerChoice) {
     else if (playerChoice.toUpperCase() == "SCISSORS") {
         switch (computerChoice) {
             case "PAPER":
-                return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ". Player wins!"
-            break;
+                return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ". Player wins the round!"
+                break;
             case "ROCK":
-                return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ". Computer wins!"
-            break;
+                return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ". Computer wins the round!"
+                break;
             default:
                 return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ". It's a tie!"
         }
@@ -45,11 +44,11 @@ function playRound (playerChoice, computerChoice) {
     else if (playerChoice.toUpperCase() == "PAPER") {
         switch (computerChoice) {
             case "ROCK":
-                return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ".. Player wins!"
-            break;
+                return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ".. Player wins the round!"
+                break;
             case "SCISSORS":
-                return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ".. Computer wins!"
-            break;
+                return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ".. Computer wins the round!"
+                break;
             default:
                 return "Player selected " + playerChoice + ". Computer selected " + computerChoice + ".. It's a tie!"
         }
@@ -59,25 +58,87 @@ function playRound (playerChoice, computerChoice) {
 
 }
 
-function getPlayerChoice () {
- return prompt("Choose Rock, Paper, or Scissors");
-} 
-
-
-function game () {
-    let computerScore = 0;
-    let playerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        displayText = playRound (getPlayerChoice(), getComputerChoice());
-        if (displayText.includes("Computer wins")) {
-            computerScore++;
-        }
-        else if (displayText.includes("Player wins")) {
-            playerScore++;
-        }
-        console.log (displayText);
-        console.log("Here are the scores. Player:" + playerScore + "; Computer:" + computerScore);
-    }
+function getPlayerChoice() {
+    return prompt("Choose Rock, Paper, or Scissors");
 }
 
-game ();
+
+const rockBtn = document.querySelector('#rock');
+rockBtn.addEventListener('click', () => {
+    playRoundResult = playRound("Rock", getComputerChoice());
+    game(playRoundResult);
+});
+
+const paperBtn = document.querySelector('#paper');
+paperBtn.addEventListener('click', () => {
+    playRoundResult = playRound("paper", getComputerChoice());
+    game(playRoundResult);
+});
+
+const scissorsBtn = document.querySelector('#scissors');
+scissorsBtn.addEventListener('click', () => {
+    playRoundResult = playRound("scissors", getComputerChoice());
+    game(playRoundResult);
+});
+
+function game(playRoundResult) {
+    let computerScore = 0;
+    let playerScore = 0;
+    const divResult = document.querySelector('#result');
+    //for (let i = 0; i < 5; i++) {
+    displayText = playRoundResult;
+    if (displayText.includes("Computer wins")) {
+        currentScore = updateScore("Computer");
+        divResult.textContent = displayText;
+        checkWinner(currentScore, "Computer")
+
+    }
+    else if (displayText.includes("Player wins")) {
+        currentScore = updateScore("Player");
+        divResult.textContent = displayText;
+        checkWinner(currentScore, "Player")
+    }
+    
+
+  //  console.log(displayText);
+  //  console.log("Here are the scores. Player:" + playerScore + "; Computer:" + computerScore);
+    // }
+}
+
+function updateScore(roundWinner) {
+const divResult = document.querySelector('#result');
+const compScore = document.querySelector('#compScore');
+const playerScore = document.querySelector('#playerScore');
+let currentCompScore = parseInt(compScore.textContent);
+let currentPlayerScore = parseInt(playerScore.textContent);
+console.log(currentCompScore);
+console.log(currentPlayerScore);
+
+if (roundWinner == "Computer") {
+    currentCompScore++;
+    compScore.textContent = currentCompScore;
+    return currentCompScore;
+} else {
+    currentPlayerScore++;
+    playerScore.textContent = currentPlayerScore;
+    return currentPlayerScore;
+}
+
+
+
+}
+
+function checkWinner(currentScore, winner) {
+const divResult = document.querySelector('#result');
+const compScore = document.querySelector('#compScore');
+const playerScore = document.querySelector('#playerScore');
+    if (currentScore == 5) {
+        if (winner = "Computer") {
+            divResult.textContent = "Game over, Computer won the game! Scores resetting to 0";
+        } else {
+            divResult.textContent = "Game over, Player won the game! Scores resetting to 0";
+        }
+        compScore.textContent = "0";
+        playerScore.textContent = "0";
+    }
+}
